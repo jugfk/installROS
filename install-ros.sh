b8826e2ffdca81284ddc22d232431ddd2406dc47
Jetson User Group For Korean <59607996+jetsonworld@users.noreply.github.com>
2020-02-17 14:39:42 +0900

    Add files via upload

---
+++ install-ros.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Reference sites
+# https://www.stereolabs.com/blog/ros-and-nvidia-jetson-nano/
+
+echo "[Add the ROS repository]"
+if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+fi
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+echo "[Update the package]"
+sudo apt-get update
+
+echo "[Installing ROS and ROS Packages]"
+sudo apt-get install -y ros-melodic-desktop
+
+echo "[rosdep init and python-rosinstall]"
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo "[Making the catkin workspace and testing the catkin build]"
+sudo apt-get install -y cmake python-rosinstall python-catkin-tools
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+source /opt/ros/melodic/setup.bash
+catkin init
+catkin build
+sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
+source ~/.bashrc
